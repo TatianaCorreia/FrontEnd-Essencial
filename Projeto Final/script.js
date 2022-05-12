5fd88047c7562b93f5501d20cc5495c7c62fdd77
Eduardo Brigatti <edubrigatti74@gmail.com>
2022-05-12 16:48:50 -0300

    trabalho final

--- Projeto Final/script.js
+++ Projeto Final/script.js
@@ -5,15 +5,6 @@ function carregar() {
     var hora = data.getHours()
     var minuto = data.getMinutes()
     
-    
            msg.innerHTML = `Hora do dia: ${hora}hs e ${minuto}min`
-        if (hora >= 0 && hora < 12) {
-            /*img.src = "manha.png"*/
-            document.body.style.background = "#e0e5ed"
-        } else if (hora >= 12 && hora < 18) {
-              document.body.style.background = "#5c597a"
-        } else {
-            /*img.src = "noite.png"*/
-            document.body.style.background = "#0b2c3c"
-        }
+        
     }
